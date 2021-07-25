06e8f1e005854d03769e22ced3b3c3a3c57467a3
sarakhatibeh <sara_basem66@yahoo.com>
2021-07-25 13:53:46 +0300

    exam

---
+++ js/apps.js
@@ -0,0 +1,34 @@
+'strict'
+
+
+function randomNum(){
+  ranNum= Math.floor(Math.random() * 500);
+    
+}
+let book=[];
+function Books(bName,bPrice){
+this.bName=bName;
+this.bPrice=bPrice;
+book.push(this);
+}
+
+
+
+
+
+
+
+
+
+
+
+let conOne=document.getElementById('one')
+let myform=document.getElementById('myForm')
+myform.addEventListener('submit',)
+// function addBook(event){
+//     event.preventDefault();
+    
+    
+// }
+let conTwo=document.getElementById('two');
+let tableE=document.getElementById('mytable');
